a69779ebfef395751cc0c16ab7cbca88f6ff44a4
Soufiane <soufiane.ouirane@acaisoft.com>
2017-05-01 02:46:28 +0200

    Add new comment

--- src/App.js
+++ src/App.js
@@ -2,34 +2,76 @@ import React, {Component} from 'react';
 import Comment from "./components/Comment";
 
 class App extends Component {
+
     state = {
-        comments: [
-            "here is a test comment1",
-            "here is a test comment2",
-            "here is a test comment3"
-        ]
+        isNewComment: false,
+        comments: []
     };
 
     updateComment = (newText, i) => {
-    let arr = this.state.comments;
-    arr[i] = newText;
-    this.setState({comments: arr});
-};
-    removeComment = (i) => {
         let arr = this.state.comments;
-        arr.splice(i,1);
+        arr[i] = newText;
         this.setState({comments: arr});
     };
-        render()
-        {
-            return (
-                <div>
-                    {this.state.comments.map((text, i) =>{
-                            return (<Comment key={i} index={i} updateComment={this.updateComment} removeComment={this.removeComment}  >{text}</Comment>)
-                        })}
+    removeComment = (i) => {
+        let arr = this.state.comments;
+        arr.splice(i, 1);
+        this.setState({comments: arr});
+    };
+
+    addComment = (newComment) => {
+        let arr = this.state.comments;
+        arr.push(newComment);
+        this.setState({comments: arr});
+    };
+    newComment = () => {
+        this.setState(prevState => ({
+            isNewComment: true
+        }))
+    };
+    saveComment = () => {
+        let newTextValue = this.refs.newText.value;
+        this.addComment(newTextValue);
+        this.setState(prevState => ({
+            isNewComment: false
+        }))
+    };
+
+    addNewCommentForm = () => {
+        return (
+            <div>
+                <div >
+                    <textarea ref="newText" defaultValue="Add new Comment"/>
+                    <button onClick={this.saveComment}>save</button>
                 </div>
-            );
+                <div>
+                    {this.state.comments.map((text, i) => {
+                        return (<Comment key={i} index={i} updateComment={this.updateComment}
+                                         removeComment={this.removeComment}>{text}</Comment>)
+                    })}
+                </div>
+            </div> )
+    };
+    normalCommentForm = () => {
+        return ( <div>
+            <button onClick={this.newComment}>New Comment</button>
+            <div>
+                {this.state.comments.map((text, i) => {
+                    return (<Comment key={i} index={i} updateComment={this.updateComment}
+                                     removeComment={this.removeComment}>{text}</Comment>)
+                })}
+            </div>
+        </div> )
+    };
+
+
+    render() {
+        if (this.state.isNewComment) {
+            return this.addNewCommentForm();
+        } else {
+            return this.normalCommentForm();
         }
     }
+}
 
-    export default App;
+export default App;
